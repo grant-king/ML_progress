5e8a7bfb5ce8fc8f3eed3d4ef98046967b1edd07
Grant King <grantryan.king@gmail.com>
2019-06-25 23:11:22 -0700

    Update motion_control2
    Add new Ball class for a sprite with a constant velocity.

--- RFCS_challenges/motion_control2.py
+++ RFCS_challenges/motion_control2.py
@@ -1,13 +1,13 @@
 import pygame
 from pygame.locals import *
 
-class Ball(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        super(Ball, self).__init__()
+        super(Player, self).__init__()
 
-        self.image = pygame.image.load('circle.png').convert()
-        self.image.set_colorkey((255, 255, 255), RLEACCEL)
-        self.position = self.image.get_rect()
+        self.surface = pygame.image.load('circle.png').convert()
+        self.surface.set_colorkey((255, 255, 255))
+        self.position = self.surface.get_rect()
         
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
@@ -19,30 +19,52 @@ class Ball(pygame.sprite.Sprite):
         if pressed_keys[K_RIGHT]:
             self.position.move_ip(5, 0)
 
-pygame.init()
-main_window = pygame.display.set_mode((500, 500))
 
-ball = Ball()
-running = True
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Ball, self).__init__()
 
-clock = pygame.time.Clock()
+        self.surface = pygame.image.load('circle.png').convert()
+        self.surface.set_colorkey((255, 255, 255))
+        self.position = self.surface.get_rect()
+        self.velocity = [3, 1]
 
-while running:
+    def update(self):
+        self.position.move_ip(self.velocity)
+
+
+def listen_quit():
     #listen for quit
+    running = True
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
         elif event.type == QUIT:
             running = False
-    
+    return running
+
+pygame.init()
+main_window = pygame.display.set_mode((500, 500))
+
+player = Player()
+ball = Ball()
+running = True
+
+clock = pygame.time.Clock()
+
+while running:
+    running = listen_quit()
     clock.tick(60)
 
     pressed_keys = pygame.key.get_pressed()
-    ball.update(pressed_keys)
+    player.update(pressed_keys)
+    ball.update()
 
     main_window.fill((0, 0, 0))
-    main_window.blit(ball.image, ball.position)
+    main_window.blit(player.surface, player.position)
+    main_window.blit(ball.surface, ball.position)
+
     pygame.display.flip()        
 
     
