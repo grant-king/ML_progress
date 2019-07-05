9f3b90fc1a24124b6234e5a9723e5920e14eac3d
Grant King <grantryan.king@gmail.com>
2019-07-04 18:34:40 -0700

    Update memory.py
    Add memory game functionality.

--- RFCS_challenges/memory/memory.py
+++ RFCS_challenges/memory/memory.py
@@ -8,16 +8,16 @@ class Card(pygame.sprite.Sprite):
         self.max_x = pygame.display.Info().current_h
         self.max_y = pygame.display.Info().current_w
         
+        self.color = [1, 1, 1]
         self.front_face = pygame.Surface([60, 60])
         self.back_face = pygame.image.load('cardback.png').convert()
-        self.active_face = self.front_face
+        self.active_face = self.back_face
 
         spawnx = random.randint(0, self.max_x)
         spawny = random.randint(0, self.max_y)
-        print(spawnx, spawny)
         self.rect = self.active_face.get_rect(center=(spawnx, spawny))
         
-        self.velocity = [1, 1]   
+        self.velocity = [0, 1]   
 
     def update(self):
         self.control()
@@ -31,15 +31,23 @@ class Card(pygame.sprite.Sprite):
         #reflect off of walls
         if self.rect.midtop[1] < 0:
             self.velocity[1] = -self.velocity[1]
+            self.rect.move_ip(0, 10)
         if self.rect.midbottom[1] > self.max_y:
             self.velocity[1] = -self.velocity[1]
+            self.rect.move_ip(0, -10)
         if self.rect.midleft[0] < 0:
             self.velocity[0] = -self.velocity[0]  
+            self.rect.move_ip(10, 0)
         if self.rect.midright[0] > self.max_x:
             self.velocity[0] = -self.velocity[0]
+            self.rect.move_ip(-10, 0)
 
     def bounce_handler(self):
         self.velocity = [-self.velocity[0], -self.velocity[1]]
+
+    def click_handler(self):
+        if self.active_face == self.back_face:
+            self.active_face = self.front_face
 
 class Match:
     def __init__(self, card_1, card_2):
@@ -53,6 +61,8 @@ class Match:
         r, g, b = [random.randint(0, 255) for i in range(3)]
         self.card_base.front_face.fill((r, g, b))
         self.card_copy.front_face.fill((r, g, b))
+        self.card_base.color = [r, g, b]
+        self.card_copy.color = [r, g, b]
         self.color = [r, g, b]
 
 def listen_quit():
@@ -65,14 +75,33 @@ def listen_quit():
             return False
     return True
 
+def get_click_pos():
+    click = pygame.mouse.get_pressed()
+    click_pos = None
+    if click[0]:
+        click_pos = pygame.mouse.get_pos()
+        #wait for mouse up
+        for event in pygame.event.get():
+            while event.type == pygame.MOUSEBUTTONDOWN:
+                print('waiting')
+    return click_pos
+
+def compare(compare_list):
+    if compare_list[0].color == compare_list[1].color:
+        return True
+    return False
+
 pygame.init()
 main_window = pygame.display.set_mode((600, 600))
-cards = [Card() for card in range(16)]
-matches = [Match(cards[i], cards[i+1]) for i in range(16)[::2]]
+cards = [Card() for card in range(6)]
+matches = [Match(cards[i], cards[i+1]) for i in range(len(cards))[::2]]
 all_sprites = pygame.sprite.Group()
 all_sprites.add(cards)
 
 running = True
+
+to_compare = []#up to two cards
+matched = []#all successfully compared, stay face up
 clock = pygame.time.Clock()
 
 while running:
@@ -80,14 +109,42 @@ while running:
     clock.tick(60)
     main_window.fill((0, 0, 0))
 
-    for entity in all_sprites:
+    click_pos = get_click_pos()
+
+    for card in all_sprites:
+        #check clicks
+        if click_pos:
+            if card.rect.collidepoint(click_pos):
+                for couples in matched:
+                    if card not in couples:
+                        card.click_handler() 
+                if card not in to_compare:
+                    to_compare.append(card)
         #check collisions
-        card_collides = pygame.sprite.spritecollideany(entity, all_sprites)
+        card_collides = pygame.sprite.spritecollideany(card, all_sprites)
         if type(card_collides) is Card:
-            entity.bounce_handler()
+            card.bounce_handler()
             card_collides.bounce_handler()
         #update sprites and draw
-        entity.update()
-        main_window.blit(entity.active_face, entity.rect)
+        card.update()
+        main_window.blit(card.active_face, card.rect)
     
+    #update display
     pygame.display.flip()
+    
+    #game logic
+    if click_pos != None:
+        if len(to_compare) == 2:
+            print(to_compare)
+            match = compare(to_compare)
+            if match:
+                matched.append(to_compare.copy())
+                print('match')
+            else:
+                to_compare[0].active_face =  to_compare[0].back_face
+                to_compare[1].active_face =  to_compare[1].back_face
+                print('no match')
+            #reset
+            to_compare.clear()
+        
+    
