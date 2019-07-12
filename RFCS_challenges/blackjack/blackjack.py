a7ea8ab056295a0f2955a154c1033f8aa5380216
Grant King <grantryan.king@gmail.com>
2019-07-12 00:17:20 -0700

    Update blackjack.py
    Add Hand and Deck classes.

--- RFCS_challenges/blackjack/blackjack.py
+++ RFCS_challenges/blackjack/blackjack.py
@@ -2,16 +2,50 @@ import pygame
 from pygame.locals import *
 import random
 
-class Card(pygame.sprite.Sprite):
-    SUITS = ('C', 'S', 'H', 'D')
-    RANKS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
-    VALUES = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+class Hand:
+    def __init__(self):
+        self.in_hand = []
 
-    def __init__(self, suit, rank):
+    def add_card(self, card):
+        self.in_hand.append(card)
+
+    @property
+    def score(self):
+        score = 0
+        for card in self.in_hand:
+            score += card.points
+
+
+class Deck:
+    SUITS = ('Clubs', 'Spades', 'Hearts', 'Diamonds')
+    RANKS = ('Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
+    
+    def __init__(self):
+        self.deck_contents = []
+        self.discarded = []
+
+        self.build_deck()
+
+    def build_deck(self):
+        for item in self.RANKS:
+            for suit in self.SUITS:
+                self.deck_contents.append(Card(item, suit))
+    
+    def discard(self, card):
+        self.discarded.append(self.deck_contents.pop(card))
+
+
+class Card(pygame.sprite.Sprite):
+    SUITS = ('Clubs', 'Spades', 'Hearts', 'Diamonds')
+    RANKS = ('Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
+    VALUES = {'Ace':1, 'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10}
+
+    def __init__(self, rank, suit):
         super(Card, self).__init__()
         
         self.suit = suit
         self.rank = rank
+        self.sheet_cell_idx = self.get_cell_idx()
 
         self.spritesheet = Spritesheet('card_deck.png', 13, 4)
         self.size = [self.spritesheet.cell_width, self.spritesheet.cell_height]
@@ -19,12 +53,18 @@ class Card(pygame.sprite.Sprite):
         self.rect = self.front_face.get_rect()
         
         #copy cell surface to front_face
-        self.spritesheet.draw(self.front_face, 0, self.rect)
+        self.spritesheet.stamp_cell(self.front_face, self.sheet_cell_idx, self.rect)
+
+    @property
+    def points(self):
+        return self.VALUES[self.rank]
 
     def __str__(self):
-        return f'{self.suit} {self.rank}'
+        return f'{self.rank} of {self.suit}, worth {self.points} points'
 
-
+    def get_cell_idx(self):
+        cell_index = self.SUITS.index(self.suit) * len(self.RANKS) + self.RANKS.index(self.rank)
+        return cell_index
 
 class Spritesheet:
     def __init__(self, filename, num_cols, num_rows):
@@ -46,11 +86,10 @@ class Spritesheet:
             #append items as rect constructor
             self.offset_list.append((x_offset, y_offset, self.cell_width, self.cell_height))
 
-    def draw(self, surface, cell_index, draw_location):
-        source = self.sheet
+    def stamp_cell(self, destination, cell_index, draw_location):
         source_area = self.offset_list[cell_index]
 
-        surface.blit(source, draw_location, area=source_area)
+        destination.blit(self.sheet, draw_location, area=source_area)
 
 
 def listen_quit(events_list):
@@ -66,19 +105,21 @@ def listen_quit(events_list):
 pygame.init()
 main_window = pygame.display.set_mode((600, 600))
 
-card = Card(1, 2)
+#card = Card('Two', 'Hearts')
+deck = Deck()
 
 running = True
 clock = pygame.time.Clock()
 
 while running:
     events = pygame.event.get()
-    clock.tick(10)
+    clock.tick(1)
     main_window.fill((0, 0, 0))
 
     running = listen_quit(events)
 
-    card_img_idx = random.randint(0, 51)
+    card = random.choice(deck.deck_contents)
+    print(card)
     main_window.blit(card.front_face, card.rect)
 
     pygame.display.flip()
