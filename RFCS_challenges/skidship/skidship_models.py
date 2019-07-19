fa734d7affdb37f8ca858a3b0922accaff129430
Grant King <grantryan.king@gmail.com>
2019-07-19 15:43:15 -0700

    Update skidship_models.py
    Implement continuous rotational and thrust controls with generalized thrust handler method.
    
    Refactor and optimize.

--- RFCS_challenges/skidship/skidship_models.py
+++ RFCS_challenges/skidship/skidship_models.py
@@ -6,6 +6,8 @@ import math
 class Ship(pygame.sprite.Sprite):
     def __init__(self):
         super(Ship, self).__init__()
+        self.ANGLE_INCREMENT = 4
+
         self.max_x = pygame.display.Info().current_w
         self.max_y = pygame.display.Info().current_h
 
@@ -21,33 +23,28 @@ class Ship(pygame.sprite.Sprite):
         self.acceleration = [0, 0]
         self.velocity = [1, 0]
         self.thrust_coeff = 1.5 #greater than one
-        self.friction_coeff = 0.01 #less than one
+        self.friction_coeff = 0.025 #less than one
 
-        self.thrust = False
-
+        self.thrusters = {
+            'thrust': False,
+            'rthrust': False,
+            'lthrust': False,
+        }
+        
     @property
     def angle_rads(self):
         return self.angle * 3.14/180
+
+    @property
+    def absolute_rotation(self):
+        #absolute clockwise rotation from original heading
+        return -(self.orient + self.angle)
 
     def update(self, events_list):
         self.control(events_list)
         self.contain()
         self.update_vectors() 
-        self.constrain_attributes()
         self.draw()
-
-    def constrain_attributes(self):
-        ACC_LIMIT = 3
-        VELOCITY_LIMIT = 20
-        
-        #constrain acceleration
-        for item in self.acceleration:
-            if item > ACC_LIMIT:
-                item = ACC_LIMIT
-        #constrain velocity
-        for item in self.velocity:
-            if item > VELOCITY_LIMIT:
-                item = VELOCITY_LIMIT
 
     def draw(self):
         main_window = pygame.display.get_surface()
@@ -58,12 +55,24 @@ class Ship(pygame.sprite.Sprite):
         self.velocity[0] *= (1 - self.friction_coeff)
         self.velocity[1] *= (1 - self.friction_coeff)
 
+        ##### fix to work with continuous 3 thruster control scheme
+            
         #update acceleration with thrust and add to velocity
-        if self.thrust:
+        if self.thrusters['thrust']:
             self.acceleration[0] = math.cos(self.angle_rads) * self.thrust_coeff
             self.acceleration[1] = math.sin(self.angle_rads) * self.thrust_coeff
         else:
             self.acceleration = [0, 0]
+
+        if self.thrusters['rthrust']:
+            self.angle -= self.ANGLE_INCREMENT
+            self.surface = pygame.transform.rotate(self.pristine_surface, self.absolute_rotation)
+
+        if self.thrusters['lthrust']:
+            self.angle += self.ANGLE_INCREMENT
+            self.surface = pygame.transform.rotate(self.pristine_surface, self.absolute_rotation)
+
+        #####
 
         self.velocity[0] += self.acceleration[0] 
         self.velocity[1] += self.acceleration[1]
@@ -71,55 +80,56 @@ class Ship(pygame.sprite.Sprite):
         #update rectangular position
         self.rect.move_ip(self.velocity)
 
-    @property
-    def absolute_rotation(self):
-        #absolute clockwise rotation from original heading
-        return -(self.orient + self.angle)
-
     def control(self, events_list):
-        ANGLE_INCREMENT = 15
-        ##todo make controls continuous, reset thrust when no input
+        keydown_inputs = {
+            K_UP: [self.toggle_thrust, 'thrust'],
+            K_LEFT: [self.toggle_thrust, 'rthrust'],
+            K_RIGHT: [self.toggle_thrust, 'lthrust'],
+        }
+
         for event in events_list:
-            if event.type == KEYDOWN:
-                if event.key == K_UP:
-                    self.thrust = True
-                elif event.key == K_DOWN:
-                    pass
-                elif event.key == K_LEFT:
-                    #rotate ccw
-                    self.angle -= ANGLE_INCREMENT
-                    self.surface = pygame.transform.rotate(self.pristine_surface, self.absolute_rotation)
-                elif event.key == K_RIGHT:
-                    #rotate clockwise
-                    self.angle += ANGLE_INCREMENT
-                    self.surface = pygame.transform.rotate(self.pristine_surface, self.absolute_rotation)
-                else:
-                    print(f'{event.key} key press')
+            if event.type == KEYDOWN or event.type == KEYUP:
+                for key_name in keydown_inputs.keys():
+                    if event.key == key_name:
+                        #execute function with command
+                        keydown_inputs[key_name][0](keydown_inputs[key_name][1])
                 print(self)
-                
-            elif event.type == KEYUP:
-                if event.key == K_UP:
-                    self.thrust = False
-                elif event.key == K_DOWN:
-                    pass
+    
+    def rotate_cw(self):
+        ANGLE_INCREMENT = 15
+        self.angle += ANGLE_INCREMENT
+        self.surface = pygame.transform.rotate(self.pristine_surface, self.absolute_rotation)
+
+    def rotate_ccw(self):
+        pass
+        
+
+    def toggle_thrust(self, thruster_name):
+        thruster_names = ['thrust', 'rthrust', 'lthrust']
+
+        if thruster_name in thruster_names:
+            for idx, thruster in enumerate(self.thrusters):
+                if self.thrusters[thruster_name] == True:
+                    self.thrusters[thruster_name] = False
+                    print('thruster off')
                 else:
-                    print(f'{event.key} key lift')
-                print(self)
+                    self.thrusters[thruster_name] = True
+                    print('thruster on')
 
     def contain(self):
-        size = 70
         #portal walls
-        ##todo change to use rect center
-        if self.rect.midtop[1] < -size:
+        if self.rect.centery < 0:
             self.rect.move_ip(0, self.max_y)
-        if self.rect.midbottom[1] > self.max_y + size:
-            self.rect.move_ip(0,  -(self.max_y + size))
-        if self.rect.midleft[0] < -size:
+        if self.rect.centery > self.max_y:
+            self.rect.move_ip(0,  -(self.max_y))
+        if self.rect.centerx < 0:
             self.rect.move_ip(self.max_x, 0)
-        if self.rect.midright[0] > self.max_x + size:
-            self.rect.move_ip(-(self.max_x + size), 0)
-
+        if self.rect.centerx > self.max_x:
+            self.rect.move_ip(-(self.max_x), 0)    
+                    
     def __str__(self):
         accx = f'{self.acceleration[0]:.2f}'
         accy = f'{self.acceleration[1]:.2f}'
-        return f'acc: {accx, accy} || vel: {self.velocity} || angle: {self.angle} || thrust: {self.thrust}'
+        return f'acc: {accx, accy} || vel: {self.velocity} || angle: {self.angle} || thrust: {self.thrusters["thrust"]}'
+
+
