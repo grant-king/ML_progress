f03f8e03ebe9f9e5a18b5c8d65558adef5d67c9a
Grant King <grantryan.king@gmail.com>
2019-07-31 23:57:09 -0700

    Update ca_1.py

--- NMS_challenges/cellular_automata/ca_1.py
+++ NMS_challenges/cellular_automata/ca_1.py
@@ -26,25 +26,26 @@ draw_cell = 0
 
 while running:
     events = pygame.event.get()
-    clock.tick(2)
-    
+    clock.tick(10)
+    print()
     running = listen_quit(events)
 
     #interaction logic
     for cell in grid.cells:
         cidx, ridx = cell.column_idx, cell.row_idx
-        neighbors = {
-            'north': grid.get_cell(cidx, ridx - 1),
-            'northeast': grid.get_cell(cidx + 1, ridx - 1),
-            'east': grid.get_cell(cidx + 1, ridx),
-            'southeast': grid.get_cell(cidx + 1, ridx + 1),
-            'south': grid.get_cell(cidx, ridx + 1),
-            'southwest':grid.get_cell(cidx - 1, ridx + 1),
-            'west': grid.get_cell(cidx - 1, ridx),
-            'northwest': grid.get_cell(cidx - 1, ridx - 1)
-        }
         try:
-            neighborhood = sum([neighbor_cell.alive for neighbor_cell in neighbors.values()])
+            #north, ne, e, se, s, sw, w, nw
+            neighbors = [
+                grid.get_cell(cidx, ridx - 1).alive,
+                grid.get_cell(cidx + 1, ridx - 1).alive,
+                grid.get_cell(cidx + 1, ridx).alive,
+                grid.get_cell(cidx + 1, ridx + 1).alive,
+                grid.get_cell(cidx, ridx + 1).alive,
+                grid.get_cell(cidx - 1, ridx + 1).alive,
+                grid.get_cell(cidx - 1, ridx).alive,
+                grid.get_cell(cidx - 1, ridx - 1).alive,
+                ]
+            neighborhood = sum(neighbors)
             if cell.alive:
                 if neighborhood < 2 or neighborhood > 3:
                     cell.alive = False
