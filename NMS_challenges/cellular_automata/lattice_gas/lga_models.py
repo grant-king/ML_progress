67a77c5b5796b7a4660ded4960491b39472ca829
Grant King <grantryan.king@gmail.com>
2019-08-09 23:05:55 -0700

    Update lga_models.py

--- NMS_challenges/cellular_automata/lattice_gas/lga_models.py
+++ NMS_challenges/cellular_automata/lattice_gas/lga_models.py
@@ -9,29 +9,23 @@ class Particle(pygame.sprite.Sprite):
         super(Particle, self).__init__()
         self.velocity = random.choice(self.VELOCITIES)
         self.location = [lat_x, lat_y] #vertex coordinates of lattice
-
-
-class Edge:
-    def __init__(self, coordinates):
-        self.coordinates = coordinates
-
+        
 
 class Lattice:
     def __init__(self, edge_spacing):
         self.WINDOW_SIZE = pygame.display.get_surface().get_size()
         self.SPACING = edge_spacing
-        num_edges = self.WINDOW_SIZE[0] // self.SPACING
-        #coordinates for vertical edge location
-        self.x_edges = [Edge([x, 0]) for x in range(0, self.WINDOW_SIZE, self.SPACING)]
-        self.y_edges = [Edge(0, y) for y in range(0, self.WINDOW_SIZE, self.SPACING)]
 
     def update(self):
         self.collision_step()
+        self.draw()
 
     def collision_step(self):
         for x_edge in self.x_edges:
             for y_edge in self.y_edges:
                 pass
                 
+    def draw(self):
+
 
 
