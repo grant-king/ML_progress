696149d9a234cd14ff9d237238319d4b969b87d3
Grant King <grantryan.king@gmail.com>
2019-09-08 00:32:47 -0700

    Update test_ttt.py
    Add tests for vertical, diagonal, and no winner.
    
    Fix test for horizontal winner.

--- RFCS_challenges/tictactoe/test_ttt.py
+++ RFCS_challenges/tictactoe/test_ttt.py
@@ -2,14 +2,33 @@ from ttt_models import *
 
 def test_board_winner_horizontal():
     board = Board()
-    board.play('x' [0, 0])
-    board.play('x' [1, 0])
-    board.play('x' [2, 0])
+    board.play('x', [0, 0])
+    board.play('x', [0, 1])
+    board.play('x', [0, 2])
 
-    board.winner() = 'x'
+    board.grader.grade()
+    assert board.winner == 'x'
 
 def test_board_winner_vertical():
-    pass
+    board = Board()
+    board.play('x', [0, 0])
+    board.play('x', [1, 0])
+    board.play('x', [2, 0])
+    
+    board.grader.grade()
+    assert board.winner == 'x'
 
 def test_board_winner_diagonal():
-    pass
+    board = Board()
+    board.play('x', [0, 0])
+    board.play('x', [1, 1])
+    board.play('x', [2, 2])
+    
+    board.grader.grade()
+    assert board.winner == 'x'
+
+def test_board_no_winner():
+    board = Board()
+    board.grader.grade()
+    assert board.winner == None
+
