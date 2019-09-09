8b207ada23d1a37c2a011b6fa52a9287b5af4e78
Grant King <grantryan.king@gmail.com>
2019-09-08 23:31:46 -0700

    Update tictactoe.py
    Add user player key handling and have computer move with MCMove.

--- RFCS_challenges/tictactoe/tictactoe.py
+++ RFCS_challenges/tictactoe/tictactoe.py
@@ -10,6 +10,29 @@ def listen_quit(events_list):
             return False
     return True
 
+def player_input(events_list, board):
+    for event in events_list:
+        if event.type == KEYDOWN:
+            if event.key == K_KP7:
+                board.play('x', [0, 0])
+            if event.key == K_KP8:
+                board.play('x', [0, 1])
+            if event.key == K_KP9:
+                board.play('x', [0, 2])
+            if event.key == K_KP4:
+                board.play('x', [1, 0])
+            if event.key == K_KP5:
+                board.play('x', [1, 1])
+            if event.key == K_KP6:
+                board.play('x', [1, 2])
+            if event.key == K_KP1:
+                board.play('x', [2, 0])
+            if event.key == K_KP2:
+                board.play('x', [2, 1])
+            if event.key == K_KP3:
+                board.play('x', [2, 2])
+
+
 SCREEN_SIZE = [300, 300]
 BACKGROUND_COLOR = [245, 240, 250]
 
@@ -28,17 +51,22 @@ while running:
     #event handlers
     running = listen_quit(events)
 
+    #display
     main_window.fill(BACKGROUND_COLOR)
+    board.update()
+    pygame.display.flip()
 
-    #game logic
-    board.random_play()
     if board.finished:
         pygame.time.delay(1000)
         board = Board()
 
-    board.update()
-    pygame.display.flip()
-
+    #game logic
+    if board.current_player == 'o':
+        best_next = MCMove(board).get_next_move()
+        board.play('o', best_next)
+    else:
+        player_input(events, board)
+    
     #delay for visual
     pygame.time.delay(100)
 
